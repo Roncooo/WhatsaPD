27ce1f3f9a689d0e170e4789b5a8102942e5ea73
Roncooo <francescooroncolatoo@gmail.com>
2024-05-15 21:20:45 +0200

    Fixed recycler view - banner

--- app/src/main/java/it/unipd/dei/esp/whatsapd/ui/home/HomeFragment.kt
+++ app/src/main/java/it/unipd/dei/esp/whatsapd/ui/home/HomeFragment.kt
@@ -1,16 +1,20 @@
 package it.unipd.dei.esp.whatsapd.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import it.unipd.dei.esp.whatsapd.Application
+import it.unipd.dei.esp.whatsapd.Poi
 import it.unipd.dei.esp.whatsapd.PoiListRecyclerViewAdapter
+import it.unipd.dei.esp.whatsapd.R
 import it.unipd.dei.esp.whatsapd.databinding.FragmentHomeBinding
 import it.unipd.dei.esp.whatsapd.ui.nearme.HomeViewModel
 import it.unipd.dei.esp.whatsapd.ui.nearme.HomeViewModelFactory
@@ -40,9 +44,39 @@ class HomeFragment : Fragment() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(activity)
 
+
         homeViewModel.allPois.observe(viewLifecycleOwner) { pois ->
-            pois.let { adapter.submitList(it) }
+            // this is necessary to let the adapter insert the banner at the beginning of the recycler view
+            // without skipping a "real" Poi
+            val dummy = Poi(
+                "", 0.0, 0.0, "", 0, true, true, true, true
+            )
+            val list = listOf(dummy) + pois
+            list.let { adapter.submitList(it) }
         }
+
+
+        view?.findViewById<SearchView>(R.id.search)
+            ?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(searchString: String?): Boolean {
+                    Log.e("fanculo fulvio", "ok")
+                    var _searchString: String = searchString ?: ""/*
+                    homeViewModel.getAllPoisByName(_searchString)
+                        .observe(viewLifecycleOwner) { pois ->
+                            pois.let { adapter.submitList(it) }
+                        }
+
+                     */
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    // Chiama la funzione per filtrare la lista quando il testo cambia
+                    //adapter.filterPoi(newText.orEmpty().trim())
+                    Log.e("fanculo fulvio", "ok")
+                    return true
+                }
+            })
 
         return root
     }
